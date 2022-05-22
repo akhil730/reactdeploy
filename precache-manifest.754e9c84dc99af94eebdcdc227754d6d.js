408e82a637ec5fb068435ecedc47eec148ea73e1
akhil <akhilg919@gmail.com>
2022-05-22 11:14:37 +0530

    Updates

---
+++ precache-manifest.754e9c84dc99af94eebdcdc227754d6d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b6403bc7ed40c693a9c",
+    "url": "/reactdeploy/static/css/main.2b289588.chunk.css"
+  },
+  {
+    "revision": "0b6403bc7ed40c693a9c",
+    "url": "/reactdeploy/static/js/main.0b6403bc.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/reactdeploy/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "2ded161d425ce65a7129",
+    "url": "/reactdeploy/static/js/2.2ded161d.chunk.js"
+  },
+  {
+    "revision": "e002a6c316b5fe276fbe",
+    "url": "/reactdeploy/static/js/runtime~main.e002a6c3.js"
+  },
+  {
+    "revision": "3272c15c579cfa75e630256ad3a8d0b6",
+    "url": "/reactdeploy/static/media/photoo.3272c15c.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/reactdeploy/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "8b5fab5cd8a8278c14f114994a0f5f12",
+    "url": "/reactdeploy/index.html"
+  }
+];
